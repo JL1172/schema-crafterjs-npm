ddb5516194f5350ea2a3928f1ba9f38804a14f09
Jacob Lang <jacoblang72@comast.net>
2024-02-19 12:31:58 -0500

    first-commit

---
+++ index.js
@@ -0,0 +1,24 @@
+class SchemaBuilder {
+    constructor() {
+
+    }
+    build(...args) {
+
+    }
+}
+
+/*
+import {SchemaBuidler, StrictString} from strict-schema"
+
+prefaced by "Strict"
+* required
+* min
+* max
+* matches
+* 
+
+const schema = SchemaBuilder.build({
+    fullName: StrictString('Name Must Be String.').StrictRequired('Name Is Required.'),
+})
+
+*/
