f53d859045e6e3ab87322561e233d06155876893
Jacob Lang <jacoblang72@comast.net>
2024-02-19 19:22:33 -0500

    disabled testing

--- main.ts
+++ main.ts
@@ -1,42 +1,42 @@
-const SchemaBuidler = require("./index");
+// const SchemaBuidler = require("./index");
 
-const schema = new SchemaBuidler();
+// const schema = new SchemaBuidler();
 
-schema.build({
-  fullName: { string: true, required: true },
-  email: { email: true },
-  username: {
-    string: true,
-    required: true,
-    min: 3,
-    max: 20,
-  },
-  password: {
-    password: true,
-    required: true,
-    min: 8,
-  },
-  age: {
-    number: true,
-    required: true,
-    min: 18,
-    max: 120,
-  },
-});
+// schema.build({
+//   fullName: { string: true, required: true },
+//   email: { email: true },
+//   username: {
+//     string: true,
+//     required: true,
+//     min: 3,
+//     max: 20,
+//   },
+//   password: {
+//     password: true,
+//     required: true,
+//     min: 8,
+//   },
+//   age: {
+//     number: true,
+//     required: true,
+//     min: 18,
+//     max: 120,
+//   },
+// });
 
-async function validation() {
-  try {
-    await schema.validate({
-      fullName: "ed",
-      email: "jacoblang127gmail.com",
-      username: "w",
-      password: "helloWorld",
-      age: 11,
-    });
-  } catch (err) {
-    console.log(err);
-  }
-}
-validation();
-// const schema_peeked = schema.peek();
-// console.log(schema_peeked);
+// async function validation() {
+//   try {
+//     await schema.validate({
+//       fullName: "ed",
+//       email: "jacoblang127gmail.com",
+//       username: "w",
+//       password: "helloWorld",
+//       age: 11,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// validation();
+// // const schema_peeked = schema.peek();
+// // console.log(schema_peeked);
