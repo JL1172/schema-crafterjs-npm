d130dfbccfa2090507271c06ee0c6a5190a57cea
Jacob Lang <jacoblang72@comast.net>
2024-02-22 22:14:02 -0500

    hopefully fixed error

--- scripts/post-install-script.js
+++ scripts/post-install-script.js
@@ -1,21 +1,19 @@
-const {keyInYNStrict} = require("readline-sync");
+const { keyInYNStrict } = require("readline-sync");
 const generate_mock_folder = require("./mock-folder-script");
 const add_custom_command = require("./custom-command-setter-script");
 
-async function package_builder() {
-    try {
-        console.log("This Is A Post-Installation Question.\n");
-        const result = keyInYNStrict("Do You Want To Generate A Mock File Showing An Example Of How To Use This Package?");
-        if (!result) {
-            console.log("Note, If You Want To Add This Option Later, Run `npm run mock-build`")
-        } else if (result) {
-            generate_mock_folder();
-        }
-    } catch (err) {
-        console.log(err);
-    } finally {
-        add_custom_command();
-    }
+console.log("This Is A Post-Installation Question.\n");
+const result = keyInYNStrict(
+  "Do You Want To Generate A Mock File Showing An Example Of How To Use This Package?"
+);
+if (!result) {
+  console.log(
+    "Note, If You Want To Add This Option Later, Run `npm run mock-build`"
+  );
+  add_custom_command();
+} else if (result) {
+  generate_mock_folder();
+  add_custom_command();
 }
 
 package_builder();
